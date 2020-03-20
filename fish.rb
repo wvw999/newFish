64630b1f9c374281da2605e79131b3eb60ef8c3b
Will Swanson <wcs97214@gmail.com>
2020-03-20 13:09:20 -0700

    new

---
+++ fish.rb
@@ -0,0 +1,9 @@
+house
+number [1,2,3,4,5]
+color ["red", "blue", "green", "white", "yellow"]
+nationality ["german", "british", "norwegian", "danish", "swedish"]
+pet ["dog", "birds", "fish", "horse", "cat"]
+smoke ["bluemaster", "pallmall", "dunhill", "blend", "prince"]
+drink ["milk", "water", "beer", "tea", "coffee"]
+
+
