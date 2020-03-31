fce972f72d430496d878133fc1b592a42cf8ca12
Will Swanson <wcs97214@gmail.com>
2020-03-31 16:32:30 -0700

    re-working clue storage and naming

--- fish.rb
+++ fish.rb
@@ -7,11 +7,8 @@ class House
         @color = {"red" => 9, "blue" => 9, "green" => 9, "white" => 9, "yellow" => 9}
         @nationality = {"german" => 9, "british" =>9, "norwegian" =>9, "danish" =>9, "swedish" =>9}
         @pet = {"dog" =>9, "birds" =>9, "fish" =>9, "horse" =>9, "cat" =>9}
-        @smoke = {"blends" =>9, "pallmall" =>9, "bluemaster" =>9, "dunhill" =>9, "prince" =>9}
+        @smoke = {"blend" =>9, "pallmall" =>9, "bluemaster" =>9, "dunhill" =>9, "prince" =>9}
         @drink = {"milk" =>9, "water" =>9, "beer" =>9, "tea" =>9, "coffee" =>9}
-    end
-    def attrs
-        instance_variables.map{|ivar| instance_variable_get ivar}
     end
 end
 
@@ -21,51 +18,57 @@ end
 @four = House.new(4)
 @five = House.new(5)
 @names = [@one, @two, @three, @four, @five]
-@clues = [:color, @nationality, @pet, @drink, @smoke]
+@attribs = [:color, :nationality, :pet, :drink, :smoke]
 
-@number_array = [[3 => "milk"], [1 => "norwegian"]]
+@neighbor_one = [[:smoke, "blend", :pet,  "cat"], [:pet, "horse", :smoke, "dunhill"], [:nationality, "norwegian", :color, "blue"]]
 
-@neighbor_one = [["blends", "cat"], ["horse", "dunhill"], ["norwegian", "blue"]]
+@neighbor_two = [:color, "green", :color, "white"]
 
-@neighbor_two = ["green", "white"]
 
-@pairs_array = [["british", "red"], ["swedish", "dog"], ["danish", "tea"], ["green", "coffee"], ["pallmall", "birds"], ["yellow", "dunhills"], ["bluemaster", "beer"], ["german", "princes"] ]
+@pairs_array = [[:nationality, "british", :color "red"], [nationality, "swedish", :pet, "dog"], [:nationality, "danish", :drink, "tea"], [:color, "green", :drink "coffee"], [:smoke, "pallmall", :pet, "birds"], [:color, "yellow", :smoke, "dunhill"], [:smoke, "bluemaster", :drink, "beer"], [:nationality, "german", :smoke, "prince"] ]
 
-def compare(a,b,c,d)
-    if a.b[c] == 1
-        a.b[d] = 1
+@neighbor_map = [[@one,@two], [@two,@one,@three], [@three,@two,@four], [@four,@three,@five], [@five,@four]]
+
+def compare(house,attrib,clueone,cluetwo)
+    if house.public_send(attrib)[clueone] == 1
+end
+
+# first two attribs are direct assignments and need no logic to locate them, just setting them directly
+def specific
+    @three.drink["milk"] = 1
+    @one.nationality["norwegian"] = 1
+end
+
+specific
+
+def both_neighbor
+    @neighbor_one.each do |clue|
+        @attribs.each do |attrib|
+        @neighbor_map.each do |location|
+            if location.length ==2
+                compare(location[0], attrib, )
+            else
+
+            end
+        end
     end
-    if a.b[c] == 0
-        a.b[d] = 0
-    end
-end    
+end
 
-def a_and_b
-    # check data pairs
-    # @test[3].color
+def pair_check
     # @test[3].color["red"] = 1
     # @test[0].color["red"] -- returns value
-    # if A, set B, & vice versa
     @pairs_array.each do |a|
         @first = a[0]
         @second = a[1]
        # binding.pry
-        @names.each do |b|
-            @clues.each do |c|
-                binding.pry
-                # puts b.c
-                # compare(b,c,@first,@second)
-                # compare(b,c,@second,@first)
+        @names.each do |house|
+            @attribs.each do |cclue|
+                compare(house,clue,@first,@second)
+                compare(house,clue,@second,@first)
             end
             # binding.pry
         end
     end
-    # @one.color.each do |key,val|
-    #     puts key
-    #     puts val
-    # end
-    # if is set to not A or not B, set pair to not
-    # binding.pry
 end
 
-@test2 = a_and_b
+@test2 = pair_check
