c3ebb3aaeaaa564f000a712142e96f28e930068e
Will Swanson <wcs97214@gmail.com>
2020-04-01 15:41:38 -0700

    working, puzzle solved in 7 loops

--- fish.rb
+++ fish.rb
@@ -18,13 +18,13 @@ end
 @four = House.new(4)
 @five = House.new(5)
 @names = [@one, @two, @three, @four, @five]
-@attribute = [:nationality, :smoke, :color, :drink, :pet]
+@attribs = [:nationality, :smoke, :color, :drink, :pet]
 
-@neighbor_one = [[:smoke, "blend", :pet,  "cat"], [:pet, "horse", :smoke, "dunhill"], [:nationality, "norwegian", :color, "blue"]]
+@neighbor_one = [[:smoke, "blend", :pet,  "cat"], [:smoke, "blend", :drink, "water"], [:pet, "horse", :smoke, "dunhill"], [:nationality, "norwegian", :color, "blue"]]
 
 @neighbor_two = [:color, "green", :color, "white"]
 
-@clue_pairs = [[:nationality, "british", :color "red"], [nationality, "swedish", :pet, "dog"], [:nationality, "danish", :drink, "tea"], [:color, "green", :drink "coffee"], [:smoke, "pallmall", :pet, "birds"], [:color, "yellow", :smoke, "dunhill"], [:smoke, "bluemaster", :drink, "beer"], [:nationality, "german", :smoke, "prince"] ]
+@clue_pairs = [[:nationality, "british", :color, "red"], [:nationality, "swedish", :pet, "dog"], [:nationality, "danish", :drink, "tea"], [:color, "green", :drink, "coffee"], [:smoke, "pallmall", :pet, "birds"], [:color, "yellow", :smoke, "dunhill"], [:smoke, "bluemaster", :drink, "beer"], [:nationality, "german", :smoke, "prince"] ]
 
 @neighbor_map = [[@one,@two], [@two,@one,@three], [@three,@two,@four], [@four,@three,@five], [@five,@four]]
 
@@ -43,19 +43,23 @@ def both_neighbor
         @val_one = clue[1]
         @attr_two = clue[2]
         @val_two = clue[3]
-        @attribs.each do |attrib|
-            @neighbor_map.each do |location|
-                if location.length == 2
-                    if location[0].public_send(@attr_one)[@val_one] == 1
-                        location[1].public_send(@attr_two)[@val_two] = 1
-                    end
-                    if location[0].public_send(@attr_two)[@val_two] == 1
-                        location[1].public_send(@attr_one)[@val_one] = 1
-                    end
-                else
-                    if location[0].public_send(@attr_one)[@val_one] == 1 && location[1].public_send(@attr_two)[@val_two] == 0 && location[2].public_send(@attr_two)[@val_two] == 9
+        @neighbor_map.each do |location|
+            if location.length == 2
+                if location[0].public_send(@attr_one)[@val_one] == 1
+                    location[1].public_send(@attr_two)[@val_two] = 1
+                end
+                if location[0].public_send(@attr_two)[@val_two] == 1
+                    location[1].public_send(@attr_one)[@val_one] = 1
+                end
+            else
+                unless location[1].public_send(@attr_two)[@val_two] == 1  || location[2].public_send(@attr_two)[@val_two] == 1
+                    if location[0].public_send(@attr_one)[@val_one] == 1 && (location[1].public_send(@attr_two).select {|k,v| v == 1}).length == 1  && location[2].public_send(@attr_two)[@val_two] == 9
                         location[2].public_send(@attr_two)[@val_two] = 1
-                    elsif location[0].public_send(@attr_one)[@val_one] == 1 && location[1].public_send(@attr_two)[@val_two] == 9 && location[2].public_send(@attr_two)[@val_two] == 0
+                    elsif location[0].public_send(@attr_one)[@val_one] == 1 && (location[2].public_send(@attr_two).select {|k,v| v == 1}).length == 1 && location[1].public_send(@attr_two)[@val_two] == 9
+                        location[1].public_send(@attr_two)[@val_two] = 1
+                    elsif location[0].public_send(@attr_one)[@val_one] == 1 && location[1].public_send(@attr_two)[@val_two] == 0  && location[2].public_send(@attr_two)[@val_two] == 9
+                        location[2].public_send(@attr_two)[@val_two] = 1
+                    elsif location[0].public_send(@attr_one)[@val_one] == 1 && location[2].public_send(@attr_two)[@val_two] == 0  && location[1].public_send(@attr_two)[@val_two] == 9
                         location[1].public_send(@attr_two)[@val_two] = 1
                     end
                 end
@@ -65,6 +69,8 @@ def both_neighbor
 end
 
 def one_neighbor
+    @one.color["white"] = 0
+    @five.color["green"] = 0
     @neighbor_two.each do |clue|
         @attr_one = clue[0]
         @val_one = clue[1]
@@ -77,6 +83,22 @@ def one_neighbor
                 @names[plus_one].color[@val_two] = 1
             elsif house.color[@val_two] == 1
                 @names[minus_one].color[@val_one] = 1
+            end
+        end
+    end
+    @names.each do |house|
+        plus_one = @names.index(house) + 1
+        minus_one = @names.index(house) - 1
+        if house.color["blue"] == 1 || house.color["red"] == 1 || house.color["yellow"] == 1
+            unless house == @one || house == @five
+                @names[plus_one].color["white"] = 0
+                @names[minus_one].color["green"] = 0
+            end
+            if house == @one
+                @names[plus_one].color["white"] = 0
+            end
+            if house == @five
+                @names[minus_one].color["green"] = 0
             end
         end
     end
@@ -99,15 +121,11 @@ end
 # additionally, if any house attribute value is 1, set it to 0 for all the other houses
 def janitor
     @names.each do |house|
-        @attribute.each do |attrib|
+        @attribs.each do |attrib|
             negatory = house.public_send(attrib).select {|k,v| v == 0}
             incomplete = house.public_send(attrib).select {|k,v| v == 9}
             complete = house.public_send(attrib).select {|k,v| v == 1}
-            if complete.length == 1 && negatory.length == 4
-                negatory.each do |key,val|
-                    house.public_send(attrib)[key] = 0
-                end
-            elsif complete.length == 1 && negatory.length < 4
+            if complete.length == 1 && negatory.length < 4
                 combined = negatory.merge(incomplete)
                 combined.each do |key,val|
                     house.public_send(attrib)[key] = 0
@@ -116,16 +134,11 @@ def janitor
                 incomplete.each_key {|key| @update = key}
                 house.public_send(attrib)[@update] = 1
             end
-        end
-    end
-    @names.each do |house|
-        @attribute.each do |attrib|
-            complete = house.public_send(attrib).select {|k,v| v == 1}
-            if complete != nil
-                complete.each_key {|key| @update = key}
+            if complete.length > 0
+                complete.each_key {|key| @attr = key}
                 @names.each do |negate|
                     if negate != house
-                        house.public_send(@update) = 0
+                        negate.public_send(attrib)[@attr] = 0
                     end
                 end
             end
@@ -134,10 +147,11 @@ def janitor
 end
 
 # first two clues are direct assignments and need no logic to locate them, just setting them directly
+# binding.pry
 @three.drink["milk"] = 1
 @one.nationality["norwegian"] = 1
 
-10.times do |cycle|
+7.times do |cycle|
     pair_check
     janitor
     one_neighbor
@@ -145,7 +159,12 @@ end
     both_neighbor
     janitor
     @names.each do |look|
-        print look
+        puts look.number
+        puts look.color
+        puts look.nationality
+        puts look.drink
+        puts look.smoke
+        puts look.pet
         puts " "
     end
 end
